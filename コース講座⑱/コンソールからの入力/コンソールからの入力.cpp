7458ac98e7744509ed364954fd564a9162b91cc9
Manas Anjalekar アンジャレカル・マナス <anjalekarmanas10@gmail.com>
2023-06-28 13:55:31 +0530

    コース講座⑱の練習プログラム

---
+++ コース講座⑱/コンソールからの入力/コンソールからの入力.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //　aという整数型変数の宣言
+    int a;
+    //　キーボードで入力
+    cout<<"数値を入力：　"<<endl;
+    cin>>a;
+    //　値を表示
+    cout<<"a = "<<a<<endl;
+    return 0;
+}
